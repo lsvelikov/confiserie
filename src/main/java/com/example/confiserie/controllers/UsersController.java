d964f6d81043e31125e6d3bfc9c5fc14e6582efa
lsvelikov <lubomirvelikov@yahoo.com>
2023-11-02 10:20:27 +0100

    Adding login page

---
+++ src/main/java/com/example/confiserie/controllers/UsersController.java
@@ -0,0 +1,15 @@
+package com.example.confiserie.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
