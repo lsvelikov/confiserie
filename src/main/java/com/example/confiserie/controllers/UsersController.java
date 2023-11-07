25531aa75c25f7f0bf924fd96ef76243e9a27fbd
lsvelikov <lubomirvelikov@yahoo.com>
2023-11-07 22:00:17 +0100

    Some more functionality

--- src/main/java/com/example/confiserie/controllers/UsersController.java
+++ src/main/java/com/example/confiserie/controllers/UsersController.java
@@ -1,8 +1,14 @@
 package com.example.confiserie.controllers;
 
+import com.example.confiserie.model.dtos.UserRegisterDto;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -16,6 +22,22 @@ public class UsersController {
     @GetMapping("/register")
     public String register() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String confirmRegister(@Valid UserRegisterDto userRegisterDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userRegisterDto", userRegisterDto)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterDto", bindingResult);
+            return "redirect:register";
+        }
+        return "redirect:login";
+    }
+
+    @ModelAttribute
+    public UserRegisterDto userRegisterDto() {
+        return new UserRegisterDto();
     }
 }
 
---
+++ src/main/java/com/example/confiserie/model/dtos/UserRegisterDto.java
@@ -0,0 +1,104 @@
+package com.example.confiserie.model.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterDto {
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String address;
+    @Email
+    @NotBlank(message = "Email cannot be empty")
+    private String email;
+    @NotNull
+    private String phoneNumber;
+    @Size(min = 2, max = 20, message = "Username length must be between 2 and 20 characters")
+    private String username;
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters")
+    @NotNull
+    private String password;
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters")
+    @NotNull
+    private String confirmPassword;
+
+    public UserRegisterDto() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserRegisterDto setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserRegisterDto setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public UserRegisterDto setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserRegisterDto setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public UserRegisterDto setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRegisterDto setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRegisterDto setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public UserRegisterDto setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        return this;
+    }
+}
